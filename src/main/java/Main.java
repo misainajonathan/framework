53927d2a759c9099823b2b787d80411bccd6bc6d
jonathan <misainajonathan@gmail.com>
2026-06-30 20:13:49 +0300

    sprint2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import controler.FrameworkServlet;
+
 public class Main {
     public static void main(String[] args){
-        
+        FrameworkServlet f = new FrameworkServlet();
+        try {
+            f.init();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
     }
 }
