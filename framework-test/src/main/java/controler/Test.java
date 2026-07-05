241cc25d297654d48b31751254fa3d90877b0486
jonathan <misainajonathan@gmail.com>
2026-07-05 14:39:42 +0300

    teste spritn2

--- framework-test/src/main/java/controler/Test.java
+++ framework-test/src/main/java/controler/Test.java
@@ -1,8 +1,13 @@
 package controler;
 
 import annotation.Controller;
+import annotation.UrlMapping;
 
 @Controller
 public class Test {
-
+    
+    @UrlMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
